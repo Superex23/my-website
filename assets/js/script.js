6d7d932bc9bc82577813530fa22195663f91e389
Superex23 <131559764+Superex23@users.noreply.github.com>
2025-04-04 22:34:28 -0500

    Updated website
    Updated html, js, and css. Added new folder for images and documents.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
+//Change Mode Button
 const toggleButton = document.getElementById("theme-toggle");
 
 toggleButton.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
+
 });
+
+//Enlarge Cert Image
+function openCertFull() {
+    document.getElementById('cert-modal').style.display = 'flex';
+}
+
+function closeCertFull() {
+    document.getElementById('cert-modal').style.dispaly = 'none';
+}
