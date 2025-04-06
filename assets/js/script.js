5f814f3a58595bce5bcb6016f64e7b2e06442cf9
Superex23 <131559764+Superex23@users.noreply.github.com>
2025-04-06 16:41:55 -0500

    Updated Website
    Updated certificate image, moved dark/light mode button to top right, added collapsible for better mobile support.

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,21 @@ function closeCertFull() {
     document.getElementById('cert-modal').style.display = 'none';
     document.getElementById('main-header').classList.remove('hidden');
 }
+
+//Collapsible
+var coll = document.getElementsByClassName("collapsible");
+
+for (var i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+
+    // safer: find the next sibling with the class `.content`
+    var content = this.nextElementSibling;
+
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
